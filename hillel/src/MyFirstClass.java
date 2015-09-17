d4dc9f2a8a02b22300329f053a4469c4c6880abf
IMulyar <igormulyar@ukr.net>
2015-09-17 21:19:55 +0300

    hello world commit

---
+++ hillel/src/MyFirstClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IMulyar on 17.09.2015.
+ */
+public class MyFirstClass {
+
+    public static void main(String[] args){
+        String helloMessage = "Hello world!";
+        System.out.println(helloMessage);
+
+        final int igorBirthYear=1990;
+        int age = 23;
+        System.out.println("I'm "+age);
+
+
+    }
+}
