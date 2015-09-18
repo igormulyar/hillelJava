5654abdae69b56ddd5b571eb67ee7b6132f286b8
igormulyar <igormulyar@ukr.net>
2015-09-18 12:59:37 +0300

    HomeWork1 v1.0

---
+++ hillel/src/HomeWork1/AboutMyself.java
@@ -0,0 +1,32 @@
+package HomeWork1;
+
+/**
+ * Created by TRUST on 18.09.2015.
+ */
+public class AboutMyself {
+
+    public static void main(String [] args){
+        String name = "Igor";
+        String surname = "Mulyar";
+        int yearOfBirth = 1990;
+        int currentYear = 2015;
+        int age = currentYear-yearOfBirth;
+
+        System.out.println(name + " " + surname);
+
+        /* alternative solution:
+        * String fullName = name+" "+surname";
+        * System.out.println(fullName);
+        *
+        * one more:
+        * System.out.print(name);
+        * System.out.print(" ");
+        * System.out.println(surname);
+        *
+        * and more:
+        *System.out.println("Igor"+" "+"Mulyar");
+         */
+        System.out.println(age);
+    }
+
+}
