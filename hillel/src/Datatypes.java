935fd3b7f53c52b9ed0d5ddf2c944437b3960e93
IMulyar <igormulyar@ukr.net>
2015-09-21 21:07:14 +0300

    21/09/2015 in class

---
+++ hillel/src/Datatypes.java
@@ -0,0 +1,24 @@
+/**
+ * Created by IMulyar on 21.09.2015.
+ */
+public class Datatypes {
+    public static void main(String[] args) {
+        int age =24;
+        int days = age*365;
+        int hours = days * 24;
+        int seconds = hours * 3600;
+        long milliseconds = seconds * 1000L;
+        System.out.println(milliseconds);
+        long longSeconds = milliseconds / 1000;
+
+        seconds = (int) longSeconds;
+       // seconds = (int) milliseconds/1000; - not correct
+
+        System.out.println("\n");
+        double d=3.45;
+        int i = 10;
+        long l = 5000000000L;
+        float f = 2.5F;
+
+    }
+}
--- hillel/src/MyFirstClass.java
+++ hillel/src/MyFirstClass.java
@@ -1,16 +1,47 @@
+import java.net.SocketPermission;
+
 /**
  * Created by IMulyar on 17.09.2015.
  */
 public class MyFirstClass {
 
     public static void main(String[] args){
+        sayHello();
+        ageCalculation();
+        int age = 30;
+        someMethod(age);
+        System.out.println();
+
+    }
+
+    private static void someMethod(int age) {
+        System.out.println(age);
+        age =50;
+    }
+
+    private static void ageCalculation() {
+        int myBirthAge = 1990;
+        int myAge=calculateAge(myBirthAge);
+        System.out.println("My age is " +myAge);
+        int alexAge = calculateAge(1981);
+        System.out.println("Alexs age is "+alexAge);
+    }
+
+    public static void sayHello(){
         String helloMessage = "Hello world!";
         System.out.println(helloMessage);
+    }
 
-        final int igorBirthYear=1990;
-        int age = 23;
-        System.out.println("I'm "+age);
+    public static int calculateAge (){
+        final int birthYear=1990;
+        int year = 2015;
+        int age = year - birthYear;
+        return age;
+    }
 
-
+    public static int calculateAge (final int birthYear){
+        int year = 2015;
+        int age = year - birthYear;
+        return age;
     }
 }
