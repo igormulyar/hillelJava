795381ae436f66d6148a43978c85cee103414950
igor <igormulyar@ukr.net>
2015-10-02 01:46:49 +0300

    homework4

---
+++ hillel/src/homeWork4/GuessTheNumber.java
@@ -0,0 +1,14 @@
+package homeWork4;
+
+import java.util.Scanner;
+
+/**
+ * Created by igor on 02.10.15.
+ */
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        int randNumb = (int) Math.random();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
