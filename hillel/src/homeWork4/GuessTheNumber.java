9f5723ad6695127ee797c9c60521f180cf7487bf
igor <igormulyar@ukr.net>
2015-10-02 20:51:22 +0300

    homework4

--- hillel/src/homeWork4/GuessTheNumber.java
+++ hillel/src/homeWork4/GuessTheNumber.java
@@ -7,8 +7,30 @@ import java.util.Scanner;
  */
 public class GuessTheNumber {
     public static void main(String[] args) {
-        int randNumb = (int) Math.random();
+        final int randNumb = (int) (Math.random()*9+1);
+        int answer;
+        int attempts = 1;
+        //System.out.println(randNumb);
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Компьютер загадал целое число от 1 до 10. Попробуйте отгадать. Ваш ответ введите ниже:");
+
+        do {
+            answer = scanner.nextInt();
+            if (answer!=randNumb){
+                System.out.println(compareInts(randNumb,answer));
+                attempts++;
+            }else{
+                System.out.println("Поздравляем! Вы угадали число с "+attempts+" попытки!");
+            }
+        } while (answer!=randNumb);
+
+    }
+
+    private static String compareInts (final int randNumb, final int answer){
+        if (randNumb>answer){
+            return "Загаданное число больше!";
+        }else
+            return "Загаданное число меньше!";
 
     }
 }
--- hillel/src/homeWork4/LuckyTicket.java
+++ hillel/src/homeWork4/LuckyTicket.java
@@ -5,29 +5,16 @@ package homeWork4;
  */
 public class LuckyTicket {
     public static void main(String[] args) {
-        int ticketsAmount = firstSolution();
-
-
-        System.out.println("В рулоне с номерами от 000001 до 999999 содержится " + ticketsAmount + " счастливых билетов.");
-
-    }
-
-    private static int secondSolution(){
-        return 0;
-    }
-
-
-
-    private static int firstSolution() {
-        // дальше - ужасный код. Выглядит ужасно.
+        // дальше - ужасный код. Выглядит ужасно, работает медленно, но в математику лезть лень
         int counter = -1; // чтобы не учитывать билет 000000
-        final int ARR_LENGTH = 10;
-        for (int a = 0; a < ARR_LENGTH; a++) {
-            for (int b = 0; a < ARR_LENGTH; a++) {
-                for (int c = 0; c < ARR_LENGTH; c++) {
-                    for (int d = 0; d < ARR_LENGTH; d++) {
-                        for (int e = 0; e < ARR_LENGTH; e++) {
-                            for (int f = 0; f < ARR_LENGTH; f++) {
+        final int TEN = 10;
+
+        for (int a = 0; a < TEN; a++) {
+            for (int b = 0; b < TEN; b++) {
+                for (int c = 0; c < TEN; c++) {
+                    for (int d = 0; d < TEN; d++) {
+                        for (int e = 0; e < TEN; e++) {
+                            for (int f = 0; f < TEN; f++) {
                                 if (a + b + c == d + e + f) {
                                     counter++;
                                 }
@@ -37,6 +24,6 @@ public class LuckyTicket {
                 }
             }
         }
-        return counter;
+        System.out.println("В рулоне с номерами от 000001 до 999999 содержится " + counter + " счастливых билетов.");
     }
 }
