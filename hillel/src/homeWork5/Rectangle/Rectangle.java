8ddb829a2fe124382eb975599224fa2fd9b9f648
igor <igormulyar@ukr.net>
2015-10-06 01:15:54 +0300

    homework5

---
+++ hillel/src/homeWork5/Rectangle/Rectangle.java
@@ -0,0 +1,36 @@
+package homeWork5.Rectangle;
+
+/**
+ * Created by igor on 06.10.15.
+ */
+public class Rectangle {
+
+    private double xSide;
+    private double ySide;
+
+    public Rectangle (double xSide, double ySide){
+        this.xSide = xSide;
+        this.ySide = ySide;
+    }
+
+    public double getxSide() {
+        return xSide;
+    }
+
+    public void setxSide(double xSide) {
+        this.xSide = xSide;
+    }
+
+    public double getySide() {
+        return ySide;
+    }
+
+    public void setySide(double ySide) {
+        this.ySide = ySide;
+    }
+
+    @Override
+    public String toString(){
+        return "Стороны прямоуольника:\nx="+xSide+";\ny="+ySide+".";
+    }
+}
---
+++ hillel/src/homeWork5/Rectangle/RectangleTask.java
@@ -0,0 +1,45 @@
+package homeWork5.Rectangle;
+
+
+/**
+ * Created by igor on 06.10.15.
+ */
+public class RectangleTask {
+    private double s;
+    private double p;
+
+    public RectangleTask(double s, double p) {
+        this.s = s;
+        this.p = p;
+    }
+
+    public boolean hasSolution() {
+        if (discriminant((0.5 * p), s) < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Rectangle getRectangle() {
+        double x;
+        double y;
+        double discr = discriminant((0.5 * p), s);
+
+        if (discr > 0) {
+            y = (-(-0.5 * p) - (Math.sqrt(discr))) / 2;
+            x = p / 2 - y;
+            return new Rectangle(x, y);
+        } else if ((int) discr == 0) {
+            y = -(-0.5 * p) / 2;
+            x = p / 2 - y;
+            return new Rectangle(x, y);
+        } else {
+            return null;
+        }
+    }
+
+    private double discriminant(final double b, double c) {
+        return (b * b) - (4 * c);
+    }
+}
