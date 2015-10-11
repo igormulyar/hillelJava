5446b667bb7425152b8da8ff2a8496fe47425b2e
igor <igormulyar@ukr.net>
2015-10-11 15:16:20 +0300

    homework6

---
+++ hillel/src/homeWork6/artificalIntelligence/Guesser.java
@@ -0,0 +1,8 @@
+package homeWork6.artificalIntelligence;
+
+/**
+ * Created by igor on 11.10.15.
+ */
+public class Guesser {
+
+}
---
+++ hillel/src/homeWork6/artificalIntelligence/Proposer.java
@@ -0,0 +1,7 @@
+package homeWork6.artificalIntelligence;
+
+/**
+ * Created by igor on 11.10.15.
+ */
+public class Proposer {
+}
