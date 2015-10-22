15a08e35c8984510320ce4c121abfb00eaef5c27
igormulyar <igormulyar@ukr.net>
2015-10-22 18:45:52 +0300

    Homework9

--- hillel/src/homeWork9/FloatCounter.java
+++ hillel/src/homeWork9/FloatCounter.java
@@ -4,5 +4,17 @@ package homeWork9;
  * Created by igor on 20.10.15.
  */
 public class FloatCounter {
+    public static void main(String[] args) {
+        float[] array = {3.12F,
+                        3.13F,
+                        1.1F,
+                        23.23F,
+                        12.01F};
+        float result = 0F;
+        for (int i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+        System.out.println(result);
+    }
 
 }
