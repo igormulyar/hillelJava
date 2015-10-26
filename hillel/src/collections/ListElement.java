6cb300e3909682980fa10b0594283da75043a182
IMulyar <igormulyar@ukr.net>
2015-10-26 20:02:44 +0200

    26/10/2015 in class

---
+++ hillel/src/collections/ListElement.java
@@ -0,0 +1,13 @@
+package collections;
+
+/**
+ * Created by IMulyar on 26.10.2015.
+ */
+public class ListElement {
+    Object value;
+    ListElement next;
+
+    public ListElement(Object value) {
+        this.value = value;
+    }
+}
