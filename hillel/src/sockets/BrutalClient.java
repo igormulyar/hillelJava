9eec5113efef08872f14ed6c4dfd2eae2a80207d
IMulyar <igormulyar@ukr.net>
2015-11-26 20:55:42 +0200

    26/11/2015 in class

---
+++ hillel/src/sockets/BrutalClient.java
@@ -0,0 +1,37 @@
+package sockets;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Created by IMulyar on 26.11.2015.
+ */
+public class BrutalClient {
+
+    public static void main(String[] args) throws IOException {
+        new BrutalClient().start();
+    }
+
+    private void start() throws IOException {
+        Socket socket = new Socket("192.168.1.157", 3111);
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        Scanner scanner = new Scanner (System.in);
+
+
+
+        while (true){
+            String line = scanner.nextLine();
+            writer.println(line);
+            writer.flush();
+
+        }
+
+        /*writer.close(); - Unreachable statement
+        socket.close();
+    */
+    }
+
+}
