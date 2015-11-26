085b07ea02e8447d92def98771b0be7e8f2fef4c
IMulyar <igormulyar@ukr.net>
2015-11-26 20:42:41 +0200

    26/11/2015 in class

---
+++ hillel/src/sockets/MultithreadServer.java
@@ -0,0 +1,29 @@
+package sockets;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by IMulyar on 26.11.2015.
+ */
+public class MultithreadServer {
+
+
+    public static void main(String[] args) throws IOException {
+        new MultithreadServer().start();
+    }
+
+    private void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3111);
+        while(true){
+            Socket socket = serverSocket.accept();
+            Thread thread = new Thread( new SocketThread (socket));
+            thread.start();
+        }
+
+
+    }
+
+
+}
---
+++ hillel/src/sockets/SocketThread.java
@@ -0,0 +1,44 @@
+package sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * Created by IMulyar on 26.11.2015.
+ */
+public class SocketThread implements Runnable {
+
+    private final Socket socket;
+
+    public SocketThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try
+                (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 Socket localSocket = socket;) {
+
+
+            while (true) {
+                String message = reader.readLine();
+
+                if (message == null) {
+                    System.out.println("disconnected");
+                    break;
+                }
+
+                System.out.println(message);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
